a17abfe0f70fe594df800efeac38bc845701a601
Pavel Kononov <woodsleaf@gmail.com>
2019-08-30 16:28:51 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback){
+  var newArray = [];
+  // Add your code below this line
+  //console.log(this);  // посмотреть данные
+  //console.log(callback);  // посмотреть функцию сallback
+
+  /*
+  // var1 easy
+  this.forEach(item => newArray.push(callback(item)));
+  */
+  
+  // var2 important "of" 
+  /*
+  for(let item of this) {
+    newArray.push(callback(item));
+  }
+  */
+  
+  return newArray;
+};
+
+var new_s = s.myMap(function(item){
+  return item * 2;
+});
+
+console.log(new_s);
